5236b0b86162546ed4bb997acb3771cd6ef395bd
sandeep barman <sandbarm@in.ibm.com>
2020-01-07 15:32:33 +0530

    Assignment 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from './note';
+import { NotesService } from './notes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   errMessage: string;
+  note: Note;
+  noteList : Array<Note>
 
+  constructor(private noteService: NotesService ) {
+    //console.log('appcomponent');
+    this.note = new Note();
+    this.noteList = [];
+  }
+
+  ngOnInit() {
+    this.noteService.getNotes().subscribe(res =>{
+      //console.log('note list from server', res);
+      this.noteList = res;
+     },
+     error => {
+       //console.log('error from server');
+       this.errMessage = error.message;
+     })
+  }
+  takeNote() {
+    this.noteList.push(this.note)
+    this.noteService.addNote(this.note).subscribe(addedNote =>{
+    },error=>{
+      let index = this.noteList.findIndex(note=> note.id === this.note.id);
+      this.noteList.splice(index, 1)
+      this.errMessage = error.message;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {MatExpansionModule} from '@angular/material/expansion'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import { FormsModule } from '@angular/forms';
+import { NotesService } from './notes.service';
+import { HttpClientModule} from '@angular/common/http'
+import {MatCardModule} from '@angular/material/card'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,9 +20,16 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [ 
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule
   ],
-  providers: [ ],
+  providers: [NotesService ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
